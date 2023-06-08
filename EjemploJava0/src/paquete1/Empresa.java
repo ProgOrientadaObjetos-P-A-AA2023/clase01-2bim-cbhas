d9075cff684c7b9ce7105e2ea02811073f5bee48
--replace-all <calderonsebastian211@gmail.com>
2023-06-08 12:53:24 -0500

    Subida 0.1.0

--- EjemploJava0/src/paquete1/Empresa.java
+++ EjemploJava0/src/paquete1/Empresa.java
@@ -1,55 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete1;
 
-/**
- *
- * @author reroes
- */
 public class Empresa {
-    
+
     private String nombre;
-    private Edificio [] edificios;
-    private Vehiculo [] vehiculos; // tipo, matricula, valor
+    private Edificio[] edificios;
+    private Vehiculo[] vehiculos; // tipo, matricula, valor
     private double costoBienesInmuebles;
     private double costoVehiculos;
     private double costoTotalBienes;
-    
-    public void establecerNombre(String m){
+
+    public Empresa(String n, Edificio[] e, Vehiculo[] v) {
+        nombre = n;
+        edificios = e;
+        vehiculos = v;
+    }
+
+    public void establecerNombre(String m) {
         nombre = m;
     }
-    
-    public void establecerEdificios(Edificio[] m){
+
+    public void establecerEdificios(Edificio[] m) {
         edificios = m;
     }
-    
-    public void establecerCostosBienesInmuebles(){
+
+    public void establecerVehiculos(Vehiculo[] v) {
+        vehiculos = v;
+    }
+
+    public void calcularCostosBienesInmuebles() {
         double suma = 0;
         for (int i = 0; i < edificios.length; i++) {
-        // for (int i = 0; i < obtenerEdificios().length; i++) {
             suma = suma + edificios[i].obtenerCosto();
         }
-        
+
         costoBienesInmuebles = suma;
     }
-    
-    public String obtenerNombre(){
+
+    public void calcularCostoVehiculos() {
+        double suma = 0;
+        for (int i = 0; i < vehiculos.length; i++) {
+            suma = suma + vehiculos[i].obtenerValor();
+        }
+
+        costoVehiculos = suma;
+    }
+
+    public void calcularCostoTotalBienes() {
+        costoTotalBienes = costoBienesInmuebles + costoVehiculos;
+    }
+
+    public String obtenerNombre() {
         return nombre;
     }
-    
-    public Edificio[] obtenerEdificios(){
+
+    public Edificio[] obtenerEdificios() {
         return edificios;
     }
-    
-    public double obtenerCostosBienesInmuebles(){
+
+    public Vehiculo[] obtenerVehiculos() {
+        return vehiculos;
+    }
+
+    public double obtenerCostosBienesInmuebles() {
         return costoBienesInmuebles;
     }
-    
+
+    public double obtenerCostoVehiculos() {
+        return costoVehiculos;
+    }
+
+    public double obtenerCostoTotalBienes() {
+        return costoTotalBienes;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         /*
         Empresa de Hojas
         Lista de Edificios
@@ -68,22 +93,37 @@ public class Empresa {
         Total de inmuebles: 71000
         
         Total bienes: 136000
-        */
-        
-        String reporte = String.format("%s\nLista de Edificio\n", 
+         */
+
+        String reporte = String.format("%s\nLista de Edificio\n",
                 obtenerNombre());
         for (int i = 0; i < edificios.length; i++) {
-            reporte = String.format("%s%d. %s (%.2f)\n", 
-                    reporte, 
-                    i+1,
-                    edificios[i].obtenerNombre().toUpperCase(), 
+            reporte = String.format("%s%d. %s (%.2f)\n",
+                    reporte,
+                    i + 1,
+                    edificios[i].obtenerNombre().toUpperCase(),
                     edificios[i].obtenerCosto());
         }
-        
-        reporte = String.format("%sTotal de inmuebles: %.2f\n", 
+
+        reporte = String.format("%sTotal de inmuebles: %.2f\n",
                 reporte,
                 costoBienesInmuebles);
-        
+
+        reporte = String.format("%s\nLista de Vehiculos\n", reporte);
+        for (int i = 0; i < vehiculos.length; i++) {
+            reporte = String.format("%s%d. %s, %s (%.2f)\n",
+                    reporte,
+                    i + 1,
+                    vehiculos[i].obtenerTipo(),
+                    vehiculos[i].obtenerMatricula(),
+                    vehiculos[i].obtenerValor());
+        }
+
+        reporte = String.format("%sTotal de vehículos: %.2f\n\nTotal bienes: %.2f",
+                reporte,
+                costoVehiculos,
+                costoTotalBienes);
+
         return reporte;
     }
 }
--- EjemploJava0/src/paquete2/Ejecutor.java
+++ EjemploJava0/src/paquete2/Ejecutor.java
@@ -1,43 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete2;
-import paquete1.Edificio;
-import paquete1.Empresa;
 
-/**
- *
- * @author reroes
- */
+import paquete1.*;
+
 public class Ejecutor {
+
     public static void main(String[] args) {
-        
-        // Crear una arreglo de 4 elementos de tipo Edificio
+
         Edificio edf1 = new Edificio("Edificio central");
         edf1.establecerCostos(20000);
-        
         Edificio edf2 = new Edificio("Edificio norte");
         edf2.establecerCostos(30000);
         Edificio edf3 = new Edificio("Edificio sur");
         edf3.establecerCostos(10000);
         Edificio edf4 = new Edificio("Edificio secundario");
         edf4.establecerCostos(5000);
-        
-        Edificio [] listaEdificios = {edf1, edf2, edf3, edf4};
-        // double c = listaEdificios[2].obtenerCosto();
-        //System.out.println(c);
-        
-        // Crear un objeto de tipo Empresa:
-        //  Nombre: Empresa de Hojas
-        //  Lista de edificio: edf1, edf2, edf3, edf4
-        Empresa miempresa = new Empresa();
-        miempresa.establecerNombre("Empresa de Hojas");
-        miempresa.establecerEdificios(listaEdificios);
-        miempresa.establecerCostosBienesInmuebles();
-        // Imprimir el costo de los bienes inmuebles de la empresa  
-        // System.out.printf("%.2f\n", miempresa.obtenerCostosBienesInmuebles());
+
+        Vehiculo v1 = new Vehiculo("Auto", "LBB0011", 10000);
+        Vehiculo v2 = new Vehiculo("Camioneta", "LCB0011", 20000);
+        Vehiculo v3 = new Vehiculo("Auto", "LBD0012", 6000);
+        Vehiculo v4 = new Vehiculo("Camioneta", "LCC0011", 10000);
+        Vehiculo v5 = new Vehiculo("Camioneta", "LDC0011", 25000);
+
+        Edificio[] listaEdificios = {edf1, edf2, edf3, edf4};
+        Vehiculo[] listaVehiculos = {v1, v2, v3, v4, v5};
+
+        Empresa miempresa = new Empresa("Empresa de Hojas", listaEdificios,
+                listaVehiculos);
+
+        miempresa.calcularCostosBienesInmuebles();
+        miempresa.calcularCostoVehiculos();
+        miempresa.calcularCostoTotalBienes();
+
         System.out.printf("%s\n", miempresa);
     }
 }
